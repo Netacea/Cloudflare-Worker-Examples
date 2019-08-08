aa4b920ba8b30d42ab3f06c19f5051b2ad3ce625
Jack <jack.scotson@netacea.com>
2019-08-08 09:47:45 +0100

    Getting ready for when v0.0.3 goes live

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Cloudflare-Worker-Examples
 
-## Usage
-Insert your API Key in the index.js file
+## Ingest Only usage
+Replace 'YOUR-API-KEY-HERE' with the API Key we have provided in the examples/IngestOnly/index.js file
 ``` 
     cd examples/IngestOnly 
     npm i
     npm run build
 ```
 
-Build output is pushed to the ./dist folder. Paste this in your cloudflare worker
+## Mitigation and Ingest usage
+Replace 'YOUR-API-KEY-HERE' with the API Key we have provided in the examples/MitigationAndIngest/index.js file
+Replace 'YOUR-SECRET-KEY-HERE' with the secret Key we have provided in the examples/MitigationAndIngest/index.js file
+``` 
+    cd examples/MitigationAndIngest 
+    npm i
+    npm run build
+```
+
+Build output is pushed to the ./dist folder. Paste this in your cloudflare worker and click update preview to test within the sandbox.
---
+++ examples/MitigationAndIngest/index.js
@@ -0,0 +1,47 @@
+/* eslint-env worker */
+/* global addEventListener:true */
+const { ATAWorker } = require('@netacea/cloudflare-worker')
+
+addEventListener('fetch', event => {
+  event.respondWith(runNetaceaWithRequest(event))
+})
+
+const worker = new ATAWorker({
+  apiKey: 'YOUR-API-KEY-HERE',
+  secretKey: 'YOUR-SECRET-KEY-HERE'
+})
+
+async function runNetaceaWithRequest (event) {
+  let response
+  let requestStartTime = Date.now()
+  let requestLength = 0
+  const mitigationServiceResponse = await worker.mitigate(event)
+  if (mitigationServiceResponse.mitigated) {
+    response = mitigationServiceResponse.response
+    response = new Response(response.body, response)
+    requestLength = Date.now() - requestStartTime
+  } else {
+    requestStartTime = Date.now()
+    response = await handleRequest(event.request, mitigationServiceResponse)
+    requestLength = Date.now() - requestStartTime
+  }
+  event.waitUntil(worker.logRequest(event, mitigationServiceResponse, response, requestLength))
+  return response
+}
+
+async function handleRequest (request, mitServiceResponse) {
+  const response = await fetch(request)
+  const headers = new Headers(response.headers)
+
+  if (mitServiceResponse.setCookie) {
+    headers.set('Set-Cookie', mitServiceResponse.setCookie)
+  }
+
+  // Custom pre-fetch logic here. Must return fetch response object from function
+
+  return new Response(response.body, {
+    headers,
+    status: response.status,
+    statusText: response.statusText
+  })
+}
